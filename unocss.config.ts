3c157151650e250089efbf76ddbac1c094756376
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2024-03-09 13:42:41 +0100

    fix: format code

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,21 +1,17 @@
-
 import { presetUno } from 'unocss';
 import presetIcons from '@unocss/preset-icons';
 import { VitePluginConfig } from '@unocss/vite';
 
 const unoCSSConfig: VitePluginConfig = {
-  presets: [
-    presetIcons(),
-    presetUno({ dark: "media" })
-  ],
-  content: {
-    pipeline: {
-      include: [
-        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html|stories.ts)($|\?)/,
-        'src/**/*.{js,ts}',
-      ],
-    }
-  }
-}
+	presets: [presetIcons(), presetUno({ dark: 'media' })],
+	content: {
+		pipeline: {
+			include: [
+				/\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html|stories.ts)($|\?)/,
+				'src/**/*.{js,ts}'
+			]
+		}
+	}
+};
 
 export default unoCSSConfig;
